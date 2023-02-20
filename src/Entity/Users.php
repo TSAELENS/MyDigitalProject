93de84343a08aeb800cdc42be505d9e2b3fd69cf
PE-DeClercq <pierreemmanuel.declercq@gmail.com>
2023-02-20 13:28:29 +0100

    Remove role user by default

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -116,7 +116,7 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface, Timest
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        // $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
