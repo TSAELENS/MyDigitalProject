005fbbdcc23bb2ca0369d6ff658b7c3756724aec
PE-DeClercq <pierreemmanuel.declercq@gmail.com>
2023-02-24 16:12:57 +0100

    Adding Newsletter entity

---
+++ src/Entity/Newsletter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsletterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NewsletterRepository::class)]
+class Newsletter
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?bool $subscribe = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function isSubscribe(): ?bool
+    {
+        return $this->subscribe;
+    }
+
+    public function setSubscribe(bool $subscribe): self
+    {
+        $this->subscribe = $subscribe;
+
+        return $this;
+    }
+}
