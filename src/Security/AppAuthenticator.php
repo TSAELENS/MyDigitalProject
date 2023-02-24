e68d2b77c3578443add4d2549f32698f958dba64
PE-DeClercq <pierreemmanuel.declercq@gmail.com>
2023-02-24 15:07:41 +0100

    Adding navbar

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -44,11 +44,9 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
+        } else {
+            return new RedirectResponse($this->urlGenerator->generate('app_home'));
         }
-
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
