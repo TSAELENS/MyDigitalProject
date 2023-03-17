57988de8442ecf840eba1c1e1c6a676e3bc4691a
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2023-03-17 10:22:03 +0100

    Ajout champs inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,6 +17,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('lastname')
+            ->add('firstname')
+            ->add('adress')
+            ->add('city')
+            ->add('country')
+            ->add('phone')
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
