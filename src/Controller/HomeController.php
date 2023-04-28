0e0540ae059774cb9868f0bb994190e1e3315373
PE-DeClercq <pierreemmanuel.declercq@gmail.com>
2023-04-28 12:13:29 +0200

    Fix HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'images' => $images->findAll();
+            'images' => $images->findAll()
         ]);
     }
 }
