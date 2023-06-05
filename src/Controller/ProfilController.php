3eaaad6c06658b29b619d7a28c32ec66cfe5d5a5
PE-DeClercq <pierreemmanuel.declercq@gmail.com>
2023-06-05 14:22:56 +0200

    Fix rechercher un tatoueur

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -6,25 +6,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UsersRepository;
-
+use ReflectionMethod;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/profils', name: 'profils')]
+    #[Route('/tatoueurs', name: 'tatoueurs')]
     public function index(UsersRepository $usersRepository): Response
     {
-        $profils = $usersRepository->findBy(['roles' => 'ROLE_TATTLER']);
+        $profils = $usersRepository->createQueryBuilder('u')
+            ->where('u.roles LIKE :role')
+            ->setParameter('role', '%ROLE_TATTLER%')
+            ->getQuery()
+            ->getResult();
         
+        $accessor = PropertyAccess::createPropertyAccessor();
         $tatoueurs = [];
+
         foreach ($profils as $profil) {
             if (in_array('ROLE_TATTLER', $profil->getRoles())) {
-                $tatoueurs[] = $profil;
+                $tatoueur = new \stdClass();
+                $tatoueur->last_name = $this->callPrivateMethod($profil, 'getLastName');
+                $tatoueur->first_name = $this->callPrivateMethod($profil, 'getFirstName');
+                $tatoueur->city = $this->callPrivateMethod($profil, 'getCity');
+                $tatoueurs[] = $tatoueur;
             }
         }
 
         return $this->render('profil/index.html.twig', [
-            'profils' => $tatoueurs,
+            'tatoueurs' => $tatoueurs,
         ]);
+    }
+
+    private function callPrivateMethod($object, $methodName)
+    {
+        $reflectionMethod = new ReflectionMethod(get_class($object), $methodName);
+        $reflectionMethod->setAccessible(true);
+        return $reflectionMethod->invoke($object);
     }
 
 }
