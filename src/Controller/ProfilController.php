eff5c0a6141d47fe03a638512522376654a2e75f
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2023-06-05 11:35:15 +0200

    Liste tatoueurs

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -7,17 +7,24 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UsersRepository;
 
+
 class ProfilController extends AbstractController
 {
     #[Route('/profils', name: 'profils')]
     public function index(UsersRepository $usersRepository): Response
     {
         $profils = $usersRepository->findBy(['roles' => 'ROLE_TATTLER']);
+        
+        $tatoueurs = [];
+        foreach ($profils as $profil) {
+            if (in_array('ROLE_TATTLER', $profil->getRoles())) {
+                $tatoueurs[] = $profil;
+            }
+        }
 
         return $this->render('profil/index.html.twig', [
-            'profils' => $profils,
+            'profils' => $tatoueurs,
         ]);
     }
-
 
 }
